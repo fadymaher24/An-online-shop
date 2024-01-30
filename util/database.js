6bc2a4eac35d4c75420f2cf11a4caad4fd017fcc
fadymaher24 <fady.maher2024@gmail.com>
2024-01-30 23:29:20 +0200

    add files

---
+++ util/database.js
@@ -0,0 +1,28 @@
+// Using mongodb
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const mongoConnect = callback => {
+  MongoClient.connect(
+    // 'ur database'
+  )
+.then(client => {
+  console.log('Connected!');
+  _db = client.db();
+  callback();
+})
+.catch(err => {
+  console.log(err);
+  throw err;
+});
+};
+
+const getDb = () => {
+if (_db) {
+return _db;
+}
+throw 'No database found!';
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
